1daa4d5195285aa8681a6f651524b7dd2c93a6c6
Olga Kalitova <nimloth693@gmail.com>
2018-03-13 16:11:49 +0900

    Adds images Closes #7

--- app/user_page.py
+++ app/user_page.py
@@ -3,6 +3,23 @@ from app.base_page_render import render_over_base_template
 from app.current_user_info import UserInfo
 from app.forms import NewPostForm, FollowForm
 from flask import request, redirect, url_for, abort
+from app.images import ImagesStorage
+from werkzeug.utils import secure_filename
+from os.path import join
+from PIL import Image
+import datetime
+
+
+def get_posts_to_show(posts):
+    posts_to_show = []    
+    for post in posts:
+        post["time"] = datetime.datetime.fromtimestamp(post["timestamp"]).strftime('%Y-%m-%d %H:%M:%S')
+        if "image_key" in post and post["image_key"] is not None:
+            images_storage = ImagesStorage()
+            image_link = images_storage.get_image(post["image_key"])
+            post["image_link"] = image_link
+        posts_to_show.append(post)
+    return posts_to_show
 
 
 @app.route('/<username>', methods=["GET", "POST"])
@@ -11,24 +28,25 @@ def user_page(username):
     if not UserInfo.check_user_exists(username):
         app.logger.debug("App is about to abort")
         abort(404)
-    app.logger.debug("User exists? Answer: %s", UserInfo.check_user_exists(username))
     # if follow button was pressed
     follow_form = FollowForm(request.form)
     if request.method == "POST" and follow_form.validate_on_submit():
         if follow_form.follow.data:
             return redirect(url_for("user_follow", username=username))
-    # page filling 
+    # page filling
     new_post_form = NewPostForm(request.form)
     current_user_username = UserInfo.get_current_user_username()
     current_user_page = False
     if username == current_user_username:
         current_user_page = True
     posts = UserInfo.get_posts(username)
+    posts_to_show = get_posts_to_show(posts)
+    app.logger.debug("Posts to show: ", posts)
     is_following = username in UserInfo.get_followings(current_user_username)
     return render_over_base_template("user_page.html",
                                      username=username,
                                      current_user_page=current_user_page,
                                      is_following=is_following,
-                                     posts=posts,
+                                     posts=posts_to_show,
                                      follow_form=follow_form,
                                      new_post_form=new_post_form)
